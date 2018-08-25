8aa6979020db352a96e6e395a1db7edbe486dcc8
Danny Lai <tocalai@gmail.com>
2018-08-25 20:11:05 +0800

    Upload project BotConnector

---
+++ CustomerService.BotConnector/QnABot.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Bot;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Threading.Tasks;
+
+namespace CustomerService.BotConnector
+{
+    public class QnABot : IBot
+    {
+        public async Task OnTurn(ITurnContext turnContext)
+        {
+            if (turnContext.Activity.Type == ActivityTypes.Message && !turnContext.Responded)
+            {
+                await turnContext.SendActivity("No QnA Maker answers were found.");
+            }
+        }
+    }
+}
---
+++ CustomerService.BotConnector/Startup.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerService.Library.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Bot.Builder.Ai.QnA;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Core.Extensions;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Builder.TraceExtensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CustomerService.BotConnector
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.Configure<Settings>(Configuration);
+
+            services.AddBot<QnABot>(options =>
+            {
+                options.CredentialProvider = new ConfigurationCredentialProvider(Configuration);
+
+                options.Middleware.Add(new CatchExceptionMiddleware<Exception>(async (context, exception) =>
+                {
+                    await context.TraceActivity("EchoBot Exception", exception);
+                    await context.SendActivity("Sorry, it looks like something went wrong!");
+                }));
+
+                var qnaEndpoint = GetQnAMakerEndpoint(Configuration);
+                var qnaMiddleware = new QnAMakerMiddleware(qnaEndpoint);
+
+                options.Middleware.Add(qnaMiddleware);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles()
+               .UseStaticFiles()
+               .UseBotFramework();
+        }
+
+        private QnAMakerEndpoint GetQnAMakerEndpoint(IConfiguration configuration)
+        {
+            Settings settings = new Settings();
+            configuration.Bind(settings);
+
+            var host = settings.QnAMaker.Host;
+            var knowledgeBaseId = settings.QnAMaker.KnowledgeBaseId;
+            var endpointKey = settings.QnAMaker.EndpointKey;
+            return new QnAMakerEndpoint
+            {
+                Host = host,
+                KnowledgeBaseId = knowledgeBaseId,
+                EndpointKey = endpointKey
+            };
+        }
+
+    }
+}
